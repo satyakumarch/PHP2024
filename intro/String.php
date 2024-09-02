e87023873e6e078f6703c4d285435b12845b744b
SatyaKumar <satyakumarchaudhary603@gmail.com>
2024-09-02 05:51:56 +0530

    update string

--- intro/String.php
+++ intro/String.php
@@ -1,21 +1,45 @@
 <?php
-$name="satya </br>";
-$greeeting="hello ,$name";
-echo $greeeting;
+// $name="satya </br>";
+// $greeeting="hello ,$name";
+// echo $greeeting;
 
-//concatenation
-$fullgreeting=$greeeting."how are you: ";
-echo $fullgreeting;
-
-
-//string length
-echo strlen($fullgreeting);
-
-//uppercase and lowercase
-echo strtoupper($fullgreeting);
-
-echo strtolower($fullgreeting);
+// //concatenation
+// $fullgreeting=$greeeting."how are you: ";
+// echo $fullgreeting;
 
 
-//substrin
+// //string length
+// echo strlen($fullgreeting);
+
+// //uppercase and lowercase
+// echo strtoupper($fullgreeting);
+
+// echo strtolower($fullgreeting);
+
+
+// //substrin
+
+
+$name="Lovely professional university";
+echo "The name of the student is ".$name;
+echo "<br>";
+echo "<br>";
+echo "The word count is ".str_word_count($name);
+echo "<br>";
+echo "<br>";
+echo str_replace("Lovely","satya",$name);
+echo "<br>";
+echo "<br>";
+echo "The length of the string is.".strlen($name);
+echo "<br>";
+echo "<br>";
+echo "The repeat of the string is".str_repeat($name,5);
+echo "<br>";
+echo "<br>";
+echo "<pre>";
+echo rtrim("    welcome to lpu   ");
+echo "</pre>";
+echo "<pre>";
+echo ltrim("    welcome to lpu   ");
+echo "</pre>";
 ?>
