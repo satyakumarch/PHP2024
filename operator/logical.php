f8541cc1194df8d493495526d110ad4eda31bc20
SatyaKumar <satyakumarchaudhary603@gmail.com>
2024-09-02 06:43:17 +0530

    logical operator

--- operator/logical.php
+++ operator/logical.php
@@ -1,10 +1,19 @@
 <?php
-$a=1;
-$b=0;
-$c=$a||$b;
-$d=$a && $b;
-$e=$a!=$b;
-echo $c ."</br>";
-echo $d. "</br>";
-echo $e. "</br>";
+$a=10;
+$b=5;
+// $c=$a||$b;
+// $d=$a && $b;
+// $e=$a!=$b;
+echo "The or operatore of $a and $b is ";
+echo var_dump($a || $b);
+echo "<br>";
+echo "<br>";
+echo "The and operatore of $a and $b is ";
+echo var_dump($a && $b);
+echo "<br>";
+echo "<br>";
+echo "The not operatore of $a and $b is ";
+echo var_dump($a!= $b);
+echo "<br>";
+echo "<br>";
 ?>
